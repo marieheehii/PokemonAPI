15b62c6cdb3fd38344815dcd6149f90987ef8154
Hannah Young <hamyoun@iu.edu>
2022-10-07 20:27:33 -0400

    Added Index pt1

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1,6 +1,22 @@
-﻿namespace Pokedex.BlazorWASM.Models
+﻿using Newtonsoft.Json;
+
+namespace Pokedex.BlazorWASM.Models
 {
     public class Pokemon
     {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+
+        [JsonProperty("base_experience")]
+        public int Experience { get; set; }
     }
 }
